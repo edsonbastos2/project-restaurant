47e4a40f7c6a9491392db0692a1d548fcf9119de
Edson Bastos <edsonbastos2@hotmail.com>
2022-01-25 00:12:39 -0300

    criando formulario de order e validação dos campos

---
+++ src/components/Order.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="order">
+    <form>
+      <label>{{ formData.name.label }}</label>
+      <input
+        type="text"
+        placeholder="Digite seu Nome"
+        v-model="formData.name.value"
+        @blur="formData.name.isValid"
+        :class="{'error': !formData.name.valid}"
+      />
+      <p class="errorMessage" v-if="!formData.name.valid">
+        {{ formData.name.errorMessage }}
+      </p>
+    </form>
+    <button class="button" @click="orderItens">Concluir pedido</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: {
+          value: '',
+          errorMessage: 'Campo obrigatório',
+          label: 'Nome',
+          valid: true,
+          isValid: () => {
+            this.formData.name.valid = this.formData.name.value.length;
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    triggerValidations() {
+      this.formData.name.isValid();
+    },
+    orderItens() {
+      this.triggerValidations();
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.order {
+  width: 720px;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 30px 50px;
+  margin: 30px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      font-size: 16px;
+      font-weight: 600;
+      margin-bottom: 5px;
+    }
+
+    .errorMessage {
+      font-size: 12px;
+      color: var(--error);
+      margin: 10px 0;
+    }
+  }
+}
+</style>
--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-        Pagamento
+    <div class="payment">
+        <div class="payment--content">
+            <router-link to="/" class="payment--go-back">↩ Voltar</router-link>
+            <Order/>
+        </div>
+        <Cart v-if="isDesktop()" />
     </div>
 </template>
 
 <script>
+import Cart from '@/components/Cart.vue';
+import Mixins from '@/mixins/mixins';
+import Order from '@/components/Order.vue'
 export default {
-    
+    mixins: [Mixins],
+    components:{
+        Cart,
+        Order
+    }
 }
 </script>
 
 <style lang="less" scoped>
+    .payment{
+        display: flex;
 
+        &--content{
+            padding: 50px;
+            flex-grow: 1;
+
+            .payment--go-back{
+                text-decoration: none;
+                color: var(--dark);
+                font-size: 18px;
+                font-weight: 600;
+            }
+        }
+
+    }
 </style>
